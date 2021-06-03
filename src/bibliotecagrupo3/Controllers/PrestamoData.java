eeb9be2c7761f2ee7901a5f01c84bb83034134a2
Rodrigo Gomez <gomezrodrigo93@gmail.com>
2021-06-03 14:41:31 -0300

    Atributos del modelo y esqueleto de metodos en controller

--- src/bibliotecagrupo3/Controllers/PrestamoData.java
+++ src/bibliotecagrupo3/Controllers/PrestamoData.java
@@ -5,10 +5,45 @@
  */
 package bibliotecagrupo3.Controllers;
 
+import bibliotecagrupo3.Models.Lector;
+import bibliotecagrupo3.Models.Prestamo;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  *
  *  Rodrigo
  */
 public class PrestamoData {
+    private Conexion conexion ;
+
+    public PrestamoData(Conexion conexion) throws SQLException {
+        this.conexion = conexion.getConexion();
+    }
     
+    public void guardarPrestamo(Prestamo prestamo){
+        
+    }
+    
+    public Prestamo buscarPrestamo(int id){
+        
+        return null;
+    }
+
+    public void borrarPrestamo(int id){
+        
+    }
+    
+    public void modificarPrestamo(Prestamo prestamo){
+        
+    }
+    
+    public ArrayList<Prestamo> getPrestamoByDate(LocalDate fecha){
+        return null;
+    }
+    
+    public ArrayList<Prestamo> getPrestamosByLector(Lector lector){
+        return null;
+    }
 }
